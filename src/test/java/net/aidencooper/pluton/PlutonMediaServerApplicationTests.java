e605aebbe3bdbcac556f40cae413857822a9083e
AidenCooper <aidencooper23@gmail.com>
2025-10-21 00:10:28 -0400

    Initial commit.

---
+++ src/test/java/net/aidencooper/pluton/PlutonMediaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aidencooper.pluton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutonMediaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
