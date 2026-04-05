247ac196a871ae5ca324cf18f976f8b29054fffa
AidenCooper <aidencooper23@gmail.com>
2026-04-05 16:19:19 -0400

    Upgrade to Spring Boot 4

---
+++ backend/src/main/java/net/aidencooper/pluton/mediaserver/video/VideoService.java
@@ -0,0 +1,10 @@
+package net.aidencooper.pluton.mediaserver.video;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+    public void stream() {
+        
+    }
+}
--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "pluton-media-server"
