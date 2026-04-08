929b1b5b88513d4ff902f13423dd8b10551367e9
AidenCooper <aidencooper23@gmail.com>
2026-04-08 17:22:58 -0400

    FFmpeg refactor

--- backend/src/main/java/net/aidencooper/pluton/mediaserver/video/VideoService.java
+++ backend/src/main/java/net/aidencooper/pluton/mediaserver/video/VideoService.java
@@ -3,6 +3,10 @@ package net.aidencooper.pluton.mediaserver.video;
 import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
 import com.github.kokorin.jaffree.ffmpeg.UrlInput;
 import com.github.kokorin.jaffree.ffmpeg.UrlOutput;
+import net.aidencooper.pluton.mediaserver.ffmpeg.FFmpegService;
+import net.aidencooper.pluton.mediaserver.ffmpeg.FFmpegUtil;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -11,7 +15,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
+@EnableAsync
 public class VideoService {
+    private FFmpegService ffmpegService;
+
+    public VideoService(FFmpegService ffmpegService) {
+        this.ffmpegService = ffmpegService;
+    }
+
     // temp placeholder
     private static final String MOVIE_PATH = System.getProperty("user.home") + "/Documents/Media/Movies";
     private static final String STREAM_PATH = System.getProperty("user.home") + "/Documents/Media/Streams";
@@ -25,29 +36,8 @@ public class VideoService {
 
         Path playlist = outputDir.resolve("master.m3u8");
 
-        if(!Files.exists(playlist)) new Thread(() -> ffmpeg(inputPath, outputDir)).start();
+        if(!Files.exists(playlist)) this.ffmpegService.transcode(inputPath, outputDir);
 
         return playlist.toString();
-    }
-
-    // inputPath = ~/Documents/Media/Movies/toystory.mp4
-    // outputDir = ~/Documents/Media/Streams/toystory
-    private void ffmpeg(Path inputPath, Path outputDir) {
-        FFmpeg.atPath(Paths.get("/opt/homebrew/bin"))
-                .addInput(UrlInput.fromPath(inputPath))
-                .addOutput(
-                        UrlOutput.toPath(outputDir.resolve("master.m3u8"))
-                                .setFormat("hls")
-                                .addArguments("-hls_time", "6") // Every .ts segment is 6 seconds long
-                                .addArguments("-hls_list_size", "0") // Keep all .ts segments
-                                .addArguments("-hls_playlist_type", "vod") // video on demand
-                                .addArguments("-hls_segment_filename", // segment_000.ts segment_001.ts
-                                        outputDir.resolve("segment_%03d.ts").toString())
-                                .addArguments("-c:v", "libx264") // H.264 video codec
-                                .addArguments("-preset", "veryfast") // encoding speed to compression efficiency ratio
-                                .addArguments("-crf", "23") // video bitrate
-                                .addArguments("-c:a", "aac") // AAC audio codec
-                                .addArguments("-b:a", "128k") // audio bitrate
-                ).execute();
     }
 }
